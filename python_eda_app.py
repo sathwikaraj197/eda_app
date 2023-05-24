d9679039ec643f452c8a2468e262cd959c3ba574
Sunny Solanki <sunny.2309@yahoo.in>
2023-05-24 17:19:04 +0530

    Added Link to Dataset

--- python_eda_app.py
+++ python_eda_app.py
@@ -40,7 +40,7 @@ def find_cat_cont_columns(df): ## Logic to Separate Continuous & Categorical Col
 st.set_page_config(page_icon=":bar_chart:", page_title="EDA Automated using Python")
 
 st.title("Harnessing the Power of Python to Automate EDA :bar_chart: :tea: :coffee:")
-st.caption("Upload CSV file to see various Charts related to EDA. Please upload file that has both continuous columns and categorical columns. Once you upload file, various charts, widgets and basic stats will be displayed. As a sample example, you can upload famous Titanic Dataset available from Kaggle.")
+st.caption("Upload CSV file to see various Charts related to EDA. Please upload file that has both continuous columns and categorical columns. Once you upload file, various charts, widgets and basic stats will be displayed. As a sample example, you can upload famous <a href='https://www.kaggle.com/competitions/titanic/data?select=train.csv'>Titanic Dataset</a> available from Kaggle.", unsafe_allow_html=True)
 st.markdown("Please feel free to visit <a href='https://www.youtube.com/watch?v=jS3uhxD-5Cs'>this video link</a> if you want to learn how to create this Web App.", unsafe_allow_html=True)
 upload = st.file_uploader(label="Upload File Here:", type=["csv"])
 
